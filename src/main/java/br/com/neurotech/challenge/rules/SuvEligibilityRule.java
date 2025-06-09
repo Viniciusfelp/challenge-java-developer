42e360da580e49f24df147fd8ac76611074d7fef
Vinícius Felipe <56048036+Viniciusfelp@users.noreply.github.com>
2025-06-09 20:29:25 -0300

    feat: Finaliza a implementação da API para o desafio de Análise de Crédito

--- src/main/java/br/com/neurotech/challenge/rules/SuvEligibilityRule.java
+++ src/main/java/br/com/neurotech/challenge/rules/SuvEligibilityRule.java
@@ -1,6 +1,5 @@
 package br.com.neurotech.challenge.rules;
 
-import br.com.neurotech.challenge.DTOs.NeurotechClientResponseDTO;
 import br.com.neurotech.challenge.entity.NeurotechClient;
 import br.com.neurotech.challenge.entity.VehicleModel;
 import org.springframework.stereotype.Component;
--- src/test/java/br/com/neurotech/challenge/service/CreditServiceTest.java
+++ src/test/java/br/com/neurotech/challenge/service/CreditServiceTest.java
@@ -1,6 +1,5 @@
 package br.com.neurotech.challenge.service;
 
-import br.com.neurotech.challenge.DTOs.ClientIncomeResponseDTO;
 import br.com.neurotech.challenge.entity.CreditType;
 import br.com.neurotech.challenge.entity.NeurotechClient;
 import br.com.neurotech.challenge.entity.VehicleModel;
@@ -13,15 +12,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
+
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
